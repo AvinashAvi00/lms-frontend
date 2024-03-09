fd70630911270dca055ddbe3dd60e106de12a54e
AvinashAvi00 <avinaskumaravi003@gmail.con>
2024-03-09 23:51:46 +0530

    Added about us page and fixed sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './Pages/HomePage';
+import AboutUs from './Pages/AboutUs';
 
 function App() {
   
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
     <Routes>
-      { <Route path="/" element={<HomePage /> } ></Route>}
+       <Route path="/" element={<HomePage /> } ></Route>
+       <Route path="/about" element={<AboutUs /> } ></Route>
     </Routes>
     
     </>
